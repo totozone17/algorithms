31691ceeeb61a30e151332bed2f75e65534a97db
mandoomh <totozone17@outlook.com>
2024-04-04 20:43:12 +0900

    삽입정렬 2번째 실패

--- src/bakjun/insertionSort/24056/index.cjs
+++ src/bakjun/insertionSort/24056/index.cjs
@@ -1,74 +1,78 @@
-const isArraySame = (_sortArray, _targetArray, startIndex, endIndex) => {
-	let isSame = true;
-
-	for (let i = startIndex; i <= endIndex; i++) {
-		if (_sortArray[i] !== _targetArray[i]) {
-			isSame = false;
-			break;
-		}
-	}
-
-	return isSame;
-}
-
 const inputs = require('fs').readFileSync('src/bakjun/insertionSort/24056/input.txt').toString().split("\n");
 const arrayLength = inputs[0];
-const sortArray = inputs[1].split(" ").map(Number);
 const targetArray = inputs[2].split(" ").map(Number);
 
-let isSame = 0;
+let sortArray = inputs[1].split(" ").map(Number);
 
-// 최초 확인.
-if (isArraySame(sortArray, targetArray, 0, arrayLength - 1)) {
-	isSame = 1;
-	console.log(isSame);
-	return;
+let sameIndexes = {};
+let sameCount = 0;
+let count = 0;
+
+for (let i = 0; i < arrayLength; i++) {
+	if (sortArray[i] === targetArray[i]) {
+		sameIndexes[i] = true;
+		sameCount++;
+	} else {
+		sameIndexes[i] = false;
+	}
 }
 
-for (let i = 1; i < arrayLength; i++) {
-	const thisItem = sortArray[i];
+if (sameCount === arrayLength) {
+	count = 1;
+} else {
+	for (let i = 1; i < arrayLength; i++) {
+		const thisItem = sortArray[i];
 
-	let isFixedSame = null;
-	let sameIndex = i + 1;
-	let j = i - 1;
+		let j = i - 1;
 
-	while (0 <= j && sortArray[j] > thisItem) {
-		sortArray[j + 1] = sortArray[j];
+		while (0 <= j && sortArray[j] > thisItem) {
+			const sortIndex = j + 1;
+			sortArray[sortIndex] = sortArray[j];
 
-		if (j + 2 === sameIndex) {
-			if (sortArray[j + 1] === targetArray[j + 1]) {
-				sameIndex = j + 1;
-
-				if (isFixedSame === null) {
-					isFixedSame = isArraySame(sortArray, targetArray, j + 2, arrayLength - 1);
+			if (sortArray[sortIndex] === targetArray[sortIndex]) {
+				if (!sameIndexes[sortIndex]) {
+					sameIndexes[sortIndex] = true;
+					sameCount++;
 				}
 
-				if (isFixedSame) {
-					if (isArraySame(sortArray, targetArray, 0, j)) {
-						isSame = 1;
-						console.log(isSame);
-						return;
-					}
-				}
-			}
-		}
-
-		j = j - 1;
-	}
-
-	if (j + 1 !== i) {
-		sortArray[j + 1] = thisItem;
-
-		if (sortArray[j + 1] === targetArray[j + 1]) {
-			if (isFixedSame) {
-				if (isArraySame(sortArray, targetArray, 0, j)) {
-					isSame = 1;
-					console.log(isSame);
+				if (sameCount === arrayLength) {
+					count = 1;
+					console.log(count);
 					return;
 				}
+			} else {
+				if (sameIndexes[sortIndex]) {
+					sameIndexes[sortIndex] = false;
+					sameCount--;
+				}
+			}
+
+			j = j - 1;
+		}
+
+		if (j + 1 !== i) {
+			const sortIndex = j + 1;
+			sortArray[sortIndex] = thisItem;
+
+			if (sortArray[sortIndex] === targetArray[sortIndex]) {
+				if (!sameIndexes[sortIndex]) {
+					sameIndexes[sortIndex] = true;
+					sameCount++;
+				}
+
+				if (sameCount === arrayLength) {
+					count = 1;
+					console.log(count);
+					return;
+				}
+			} else {
+				if (sameIndexes[sortIndex]) {
+					sameIndexes[sortIndex] = false;
+					sameCount--;
+				}
 			}
 		}
 	}
 }
 
-console.log(isSame);
+console.log(count);
